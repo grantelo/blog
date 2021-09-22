ac180ac25b51af3476589222f421e4eaeffcba91
grant <grant.galaxys10e@gmail.com>
2021-09-22 13:55:08 +0300

    finally refreshToken

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/guards/local-auth.guard';
-import {JwtAuthGuard} from "./auth/guards/jwt-auth.guard";
-
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-}
